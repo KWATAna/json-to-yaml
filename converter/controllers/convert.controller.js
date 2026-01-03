967ebac2e1bdd108d517036e9cb5573816572bba
Vladyslav Bilotskyi <hypeshithype@gmail.com>
2026-01-03 10:55:00 +0200

    added custom tracing logs for converter

--- converter/controllers/convert.controller.js
+++ converter/controllers/convert.controller.js
@@ -1,19 +1,27 @@
+import process from "node:process";
+import { trace } from "@opentelemetry/api";
 import { stringify } from "yaml";
 
+const tracer = trace.getTracer(process.env.OTEL_SERVICE_NAME);
+
 function convertToYAML(req, reply) {
-	let body;
+  let body;
 
-	try {
-		body = JSON.parse(req.body.json);
-	} catch (err) {
-		req.log.error(err, "Parsing JSON body failed");
+  try {
+    const span = tracer.startSpan("parse json");
+    body = JSON.parse(req.body.json);
+    span.end();
+  } catch (err) {
+    req.log.error(err, "Parsing JSON body failed");
 
-		return reply.status(400).send("Invalid JSON input");
-	}
+    return reply.status(400).send("Invalid JSON input");
+  }
 
-	const yaml = stringify(body);
+  const span = tracer.startSpan("convert json to yaml");
+  const yaml = stringify(body);
+  span.end();
 
-	reply.send(yaml);
+  reply.send(yaml);
 }
 
 export { convertToYAML };
